8474c91fc42c5cbcc1052789609baf2ee7503ebf
Kaustubh Jaitapkar <kaustubhjaitapkar2004@gmail.com>
2026-04-08 10:18:15 +0530

    first commit

---
+++ image.bat
@@ -0,0 +1,22 @@
+@echo off
+
+REM Go to your project folder
+cd /d "C:\Users\YourName\Projects\MyFolder"
+
+REM Initialize git (only first time, harmless if already done)
+git init
+
+REM Add remote repo (only first time)
+git remote add origin https://github.com/KaustubhJaitapkar/CF-Assignment.git
+
+REM Add all files
+git add .
+
+REM Commit changes
+git commit -m "Auto upload from batch file"
+
+REM Push to GitHub
+git branch -M main
+git push -u origin main
+
+pause
